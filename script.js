0f1e66bc304cf048f67e60724737002eb093ea3c
xav <xavs421@gmail.com>
2022-05-31 12:45:46 -0400

    Add detection of read book

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 let overlay = document.querySelector('.overlay');
 let container = document.querySelector('.container');
 let cardTemplate = document.querySelector('.card.hidden');
-let addButton = document.querySelector('.add-button');
+let addButton = document.querySelector('.add-btn');
+let readButton = document.queryCommandIndeterm('.read-btn')
 let library = [];
+
+init();
+
+function init() {
+    let form = document.querySelector('form');
+    form.addEventListener('click', e => e.stopPropagation());
+    overlay.addEventListener('click', closeForm);
+
+    readButton.addEventListener('click', e => {
+        console.log(e);
+    });
+}
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -13,31 +26,24 @@ function Book(title, author, pages, read) {
 
 function addBook(book) {
     library.push(book);
+    appendBook(book);
 }
 
-function displayBooks() {
-    removeAllChild(container);
+function appendBook(book) {
+    let newCard = cardTemplate.cloneNode(true);
+    let title = newCard.querySelector('.title');
+    let author = newCard.querySelector('.author');
+    let pages = newCard.querySelector('.pages');
+    let read = newCard.querySelector('button');
 
-    library.forEach(book => {
-        let newCard = cardTemplate.cloneNode(true);
-        let title = newCard.querySelector('.title');
-        let author = newCard.querySelector('.author');
-        let pages = newCard.querySelector('.pages');
-        let read = newCard.querySelector('.read');
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages + " pages";
+    if(book.read) read.classList.add('true');
+    else read.textContent = "Not read";
 
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pages.textContent = book.pages + " pages";
-
-        newCard.classList.remove('hidden');
-        container.appendChild(newCard);
-    })
-}
-
-function removeAllChild(container) {
-    while (container.lastElementChild != addButton) {
-        container.removeChild(container.lastElementChild);
-    }
+    newCard.classList.remove('hidden');
+    container.appendChild(newCard);
 }
 
 function submitForm() {
@@ -47,20 +53,29 @@ function submitForm() {
     let title = form.elements.title.value;
     let author = form.elements.author.value;
     let numpg = form.elements.numpg.value;
-    let read = form.elements.read.value;
+    let read = form.elements.read.checked;
     let book = new Book(title, author, numpg, read);
 
     addBook(book);
-    displayBooks();
-
-    form.elements.title.value = '';
-    form.elements.author.value = '';
-    form.elements.numpg.value = '';
-    form.elements.read.value = false;
-
-    overlay.classList.add('hidden');
+    closeForm();
 }
 
 function openForm() {
     overlay.classList.remove('hidden');
+}
+
+function closeForm() {
+    document.forms.newbook.reset();
+    overlay.classList.add('hidden');
+}
+
+function refreshBooks() {
+    removeAllChild(container);
+    library.forEach(book => appendBook(book));
+}
+
+function removeAllChild(container) {
+    while (container.lastElementChild != addButton) {
+        container.removeChild(container.lastElementChild);
+    }
 }
